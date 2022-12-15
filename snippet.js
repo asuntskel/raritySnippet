2bcfeb013076df6e3e8287be6117b38b0ddebf11
asuntskel <35467428+asuntskel@users.noreply.github.com>
2022-12-15 12:23:25 -0500

    broken query repair
    changes to website was causing TypeError, fixed code to getthe values correctly.

---
+++ snippet.js
@@ -0,0 +1,11 @@
+attributes = []
+for (i=0; i<9; i++){
+    let attribute = document.querySelector(`#__next > main > div > div > div > section.info.common-spacing > div > div.rhs > div > div > div > a:nth-child(${i+1}) > div > span:nth-child(3)`).innerText.slice(0, 5).replace("%","")
+    attributes.push(parseFloat(attribute));
+}
+let lo = Math.min(...attributes);
+let avg = Math.trunc(attributes.reduce((a, b) => (a + b)) / attributes.length);
+let hi = Math.max(...attributes);
+
+console.log(attributes)
+document.querySelector('#__next > main > div > div > div > section.section-nft-details > div.dynamic-area > div.dynamic-area__header > span').innerHTML += `<br><div style="padding-left:5px;background-color:black;color:white;border: 1px solid black; width: 300px">Average: ${avg}%<br>Low: ${lo}% <br>High: ${hi}%<div>`;
