d0d2670a7e6a5364721145fa6ba5f0acaa262b66
asuntskel <35467428+asuntskel@users.noreply.github.com>
2022-12-15 12:42:28 -0500

    dealing with magic numbers
    remove magic number by counting attributes before looping

--- snippet.js
+++ snippet.js
@@ -1,8 +1,10 @@
-const nft_number = document.querySelector('#__next > main > div > div > div > section.section-nft-details > div.dynamic-area > div.dynamic-area__header > span').innerText
+const nftNumber = document.querySelector('#__next > main > div > div > div > section.section-nft-details > div.dynamic-area > div.dynamic-area__header > span').innerText
+const attributeCount = document.getElementsByClassName('attribute').length
 
-if(nft_number.length<=12){
+
+if(nftNumber.length<=12){
     attributes = []
-    for (i=0; i<9; i++){
+    for (i=0; i<attributeCount; i++){
         let attribute = document.querySelector(`#__next > main > div > div > div > section.info.common-spacing > div > div.rhs > div > div > div > a:nth-child(${i+1}) > div > span:nth-child(3)`).innerText.slice(0, 5).replace("%","")
         attributes.push(parseFloat(attribute));
     }
@@ -12,5 +14,5 @@ if(nft_number.length<=12){
     
     console.log(attributes)
 
-    document.querySelector('#__next > main > div > div > div > section.section-nft-details > div.dynamic-area > div.dynamic-area__header > span').innerHTML = nft_number + `<br><div style="padding-left:5px;background-color:black;color:white;border: 1px solid black; width: 300px">Average: ${avg}%<br>Low: ${lo}% <br>High: ${hi}%<div>`;
+    document.querySelector('#__next > main > div > div > div > section.section-nft-details > div.dynamic-area > div.dynamic-area__header > span').innerHTML = nftNumber + `<br><div style="padding-left:5px;background-color:black;color:white;border: 1px solid black; width: 300px">Average: ${avg}%<br>Low: ${lo}% <br>High: ${hi}%<div>`;
 }
